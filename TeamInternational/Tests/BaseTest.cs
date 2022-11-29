d75ff2b0ce3cf3fdcc645507c21af44847f04512
Eric de la Cruz <eric@binagora.com>
2022-11-29 07:01:34 -0300

    Update BaseTest.cs

--- TeamInternational/Tests/BaseTest.cs
+++ TeamInternational/Tests/BaseTest.cs
@@ -71,11 +71,13 @@ namespace TeamInternational.Tests
         }
 
         //This part is just to make it visible for the video, remove when done
+        /*
         [TearDown]
         protected static void MakeEndingVisibleforAWhile()
         {
             Thread.Sleep(5 * 1000);
         }
+        */
 
         public static IWebDriver Driver { get => driver; set => driver = value; }
         public static ExtentTest Test { get => test; set => test = value; }
